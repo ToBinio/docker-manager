59f9ea70b99b7238106ee76978fb40c5279233e0
to_binio <tobias.frischmann1@gmail.com>
2022-09-26 09:04:37 +0200

    angular init

---
+++ src/test/java/at/tobinio/dockermanager/DockerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.tobinio.dockermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
